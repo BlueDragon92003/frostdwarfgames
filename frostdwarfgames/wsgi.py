4d14bb25674e35d37b94279cea8258df870d40e1
Quentin <quentin@theschultzfamily.us>
2022-02-20 23:10:08 +0000

    Slowly figuring out how Django hosting works

--- frostdwarfgames/wsgi.py
+++ frostdwarfgames/wsgi.py
@@ -7,6 +7,32 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# New WSGI That doesn't work
+# Add/remove quote to switch--> """
+import sys
+
+sys.path.insert(0, '/usr/local/django/frostdwarfgames')
+
+import settings
+
+import django.core.management
+django.core.management.setup_environ(settings)
+utility = django.core.management.ManagementUtility()
+command = utility.fetch_command('runserver')
+
+command.validate()
+
+import django.conf
+import django.utils
+
+django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+
+# Old WSGI that doesn't work
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +40,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frostdwarfgames.settings')
 
 application = get_wsgi_application()
+#"""
